2d2f9eca9cdaca64a5f2df07e4a71010f55515c1
Conor McCormack <c-mack@Conors-MacBook-Pro.local>
2021-12-17 14:49:18 +0000

    index-update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const workoutRouter = require("./resources/workouts/router")
 
 app.disable("x-powered-by")
 
-app.use(cors())
+app.use(cors({origin: 'https://cmackgym.herokuapp.com'}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev"))
